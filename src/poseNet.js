08b1bc6cd6d3112e62e656492f3177b2744f0f3e
Paulo Rozatto <rozatto.paulo.victot@gmail.com>
2022-03-08 10:06:08 -0300

    counting before start

--- src/poseNet.js
+++ src/poseNet.js
@@ -1,12 +1,6 @@
 
 import * as poseDetection from '@tensorflow-models/pose-detection';
-// import * as tf from '@tensorflow/tfjs-core'
 import '@tensorflow/tfjs-backend-webgl';
-// import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
-
-// tfjsWasm.setWasmPaths(
-//     `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/`);
-
 
 const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet);
 
@@ -17,8 +11,12 @@ const ctx = canvas.getContext("2d");
 
 ctx.lineWidth = 3;
 
+let hasntStarted = true;
+
 export default async function init() {
     await startVideo(video);
+
+    tempVideo();
 
     video.width = video.videoWidth;
     video.height = video.videoHeight;
@@ -26,9 +24,34 @@ export default async function init() {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
-    canvas.style.cssText = 'width: auto; height: 100vh; overflow: hidden;'
+    let time = 6;
+    let counter = document.getElementById('counter');
 
-    processVideo();
+    let btn = document.getElementById('btn');
+    btn.style.display = 'inline';
+    btn.onclick = () => {
+        btn.style.display = 'none';
+        counter.style.display = 'inline'
+        count();
+    }
+
+    function count() {
+        if (--time > 0) {
+            counter.innerText = time.toString();
+            setTimeout(count, 1000);
+        }
+        else {
+            counter.style.display = 'none';
+            processVideo();
+        }
+    }
+}
+
+function tempVideo() {
+    ctx.drawImage(video, 0, 0)
+
+    if (hasntStarted)
+        requestAnimationFrame(tempVideo);
 }
 
 async function processVideo() {
@@ -40,38 +63,39 @@ async function processVideo() {
     ctx.strokeStyle = 'White';
     ctx.lineWidth = 1;
 
-    poseDetection.util.getAdjacentPairs(poseDetection.SupportedModels.MoveNet).forEach(([
-        i, j
-    ]) => {
-        const kp1 = pose.keypoints[i];
-        const kp2 = pose.keypoints[j];
+    if (pose?.keypoints)
+        poseDetection.util.getAdjacentPairs(poseDetection.SupportedModels.MoveNet).forEach(([
+            i, j
+        ]) => {
+            const kp1 = pose.keypoints[i];
+            const kp2 = pose.keypoints[j];
 
-        // If score is null, just show the keypoint.
-        const score1 = kp1.score != null ? kp1.score : 1;
-        const score2 = kp2.score != null ? kp2.score : 1;
-        const scoreThreshold = 0.5;
+            // If score is null, just show the keypoint.
+            const score1 = kp1.score != null ? kp1.score : 1;
+            const score2 = kp2.score != null ? kp2.score : 1;
+            const scoreThreshold = 0.5;
 
-        if (score1 >= scoreThreshold) {
-            const circle = new Path2D();
-            circle.arc(kp1.x, kp1.y, 3, 0, 2 * Math.PI);
-            ctx.fill(circle);
-            ctx.stroke(circle);
-        }
+            if (score1 >= scoreThreshold) {
+                const circle = new Path2D();
+                circle.arc(kp1.x, kp1.y, 3, 0, 2 * Math.PI);
+                ctx.fill(circle);
+                ctx.stroke(circle);
+            }
 
-        if (score2 >= scoreThreshold) {
-            const circle = new Path2D();
-            circle.arc(kp2.x, kp2.y, 3, 0, 2 * Math.PI);
-            ctx.fill(circle);
-            ctx.stroke(circle);
-        }
+            if (score2 >= scoreThreshold) {
+                const circle = new Path2D();
+                circle.arc(kp2.x, kp2.y, 3, 0, 2 * Math.PI);
+                ctx.fill(circle);
+                ctx.stroke(circle);
+            }
 
-        if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
-            ctx.beginPath();
-            ctx.moveTo(kp1.x, kp1.y);
-            ctx.lineTo(kp2.x, kp2.y);
-            ctx.stroke();
-        }
-    });
+            if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
+                ctx.beginPath();
+                ctx.moveTo(kp1.x, kp1.y);
+                ctx.lineTo(kp2.x, kp2.y);
+                ctx.stroke();
+            }
+        });
 
     requestAnimationFrame(processVideo);
 }
