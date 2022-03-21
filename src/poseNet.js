40cd8b880423ab10df10a6456f4599cbde331f70
Paulo Rozatto <rozatto.paulo.victot@gmail.com>
2022-03-21 13:14:19 -0300

    drawing all angles

--- src/poseNet.js
+++ src/poseNet.js
@@ -3,19 +3,25 @@ import * as poseDetection from '@tensorflow-models/pose-detection';
 import '@tensorflow/tfjs-backend-webgl';
 
 const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet);
+const scoreThreshold = 0.5;
 
 const video = document.getElementById('video');
 
 const canvas = document.getElementById("output");
 const ctx = canvas.getContext("2d");
-
 ctx.lineWidth = 3;
 
 let hasntStarted = true;
 
 const joints = [
-    [10, 8, 6],
-    [5, 7, 9]
+    [10, 8, 6], // right elbow
+    [9, 7, 5], // left elbow
+    [16, 14, 12], // right knee
+    [15, 13, 11], // left knee
+    [8, 6], // right shoulder
+    [7, 5], // left shoulder
+    [14, 12], // right hip
+    [13, 11], // left hip
 ];
 
 let pause = false;
@@ -23,7 +29,7 @@ window.onkeydown = (e) => {
     if (e.key == 'p')
         pause = !pause;
 
-    if(!pause)
+    if (!pause)
         requestAnimationFrame(processVideo)
 }
 
@@ -95,7 +101,6 @@ async function processVideo() {
             // If score is null, just show the keypoint.
             const score1 = kp1.score != null ? kp1.score : 1;
             const score2 = kp2.score != null ? kp2.score : 1;
-            const scoreThreshold = 0.5;
 
             if (score1 >= scoreThreshold) {
                 const circle = new Path2D();
@@ -119,37 +124,45 @@ async function processVideo() {
             }
         });
 
-        for (let i = 0; i < 1; i++) {
-            const arc = new Path2D();
-            let center = pose.keypoints[joints[i][1]];
-            let v1 = sub(pose.keypoints[joints[i][0]], center);
-            let v2 = sub(pose.keypoints[joints[i][2]], center);
-
-            let p1 = pose.keypoints[joints[i][0]];
-            let p2 = pose.keypoints[joints[i][2]]
-
-            let radians = Math.acos(
-                (v1.x * v2.x + v1.y + v2.y) / (norm(v1) * norm(v2))
-            );
-
-            
-            // console.log(radians * 180 / Math.PI)
-            
-            // let th2 = Math.atan2(center.y - v2.y, center.x - v2.x);
-            // inverter eixo y por causa dos eixos do canvas
-            let th2 = Math.atan2(p2.y - center.y, p2.x - center.x);
-            
-            let invert = false;
-
-            if(p1.y < center.y) {
-                invert = true;
-                radians *= -1;
+        for (let i = 0; i < joints.length; i++) {
+            if (pose.keypoints[joints[i][0]].score < scoreThreshold || pose.keypoints[joints[i][1]].score < scoreThreshold) {
+                continue;
             }
 
-            ctx.strokeStyle = 'Green';
-            ctx.lineWidth = 2;
-            arc.arc(center.x, center.y, 20, th2, radians, invert)
-            ctx.stroke(arc);
+            let center = pose.keypoints[joints[i][1]];
+
+            let radians = 0;
+
+            if (i < 4) {
+                if (pose.keypoints[joints[i][2]].score < scoreThreshold) {
+                    continue;
+                }
+
+                let v1 = sub(pose.keypoints[joints[i][0]], center);
+                let v2 = sub(pose.keypoints[joints[i][2]], center);
+
+                radians = Math.acos(
+                    (v1.x * v2.x + v1.y + v2.y) / (norm(v1) * norm(v2))
+                );
+            }
+            else {
+                let p1 = pose.keypoints[joints[i][0]]
+                radians = Math.atan2(p1.y - center.y, -Math.abs(p1.x - center.x))
+                // radians = Math.atan2(center.y - p1.y, center.x - p1.x)
+
+                if (radians < 0) {
+                    radians += 2 * Math.PI;
+                }
+
+                radians -= Math.PI / 2
+            }
+
+            let offset = i % 2 == 0 ? -40 : 10;
+
+            ctx.font = "20px Arial";
+            ctx.fillStyle = 'White';
+            ctx.fillText(`${(radians * 180 / Math.PI).toFixed(0)}\u00B0`, center.x + offset, center.y + 15);
+
         }
     }
 
